9ba772af85bf9f8b8455f056ce4ad0871febd3eb
eugene.sergeev@p2h.com <eugene.sergeev@p2h.com>
2024-07-13 17:29:17 +0300

    add color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ export default {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        main: {
+          white: '#FFFFFF',
+          black: '#000F30'
+        },
+        neutral: {
+          light: {
+            100: '#D3DFEB',
+            60: '#E5ECF3'
+          },
+          dark: {
+            80: '#6C869F'
+          }
+        }
+      }
+    }
   },
   variants: {
     extend: {}
